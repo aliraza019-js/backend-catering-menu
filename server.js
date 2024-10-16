bf571e9daca4f95bb9034c518e4b0825ce4f53f8
Ali Raza <admin@admins-MacBook-Air.local>
2024-10-16 23:29:08 +0500

    backend cors added

--- server.js
+++ server.js
@@ -241,4 +241,4 @@ app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, r
 });
 
 
-app.listen(3002, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
